53257df0cd07d804b84e897aa576369c202ce8bb
Bikesh Shakya Banda <bikesh.s.banda@gmail.com>
2024-01-21 16:32:45 +0545

    update states

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -15,31 +15,15 @@ const scrapeLogic = async (res) => {
         : puppeteer.executablePath(),
   });
   try {
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    
+    await page.goto(`https://ijli.com.np`, { waitUntil: 'networkidle0' });
+    const html = await page.content();
 
-    await page.goto("https://developer.chrome.com/");
+    await browser.close();
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    res.send(html);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
